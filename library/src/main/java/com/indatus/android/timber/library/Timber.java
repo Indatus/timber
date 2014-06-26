994e91227a6b283435f035f99234e6530865596a
Jon Staff <jstaff@indatus.com>
2014-06-26 18:27:08 -0400

    Added documentation to Timber.java

--- library/src/main/java/com/indatus/android/timber/library/Timber.java
+++ library/src/main/java/com/indatus/android/timber/library/Timber.java
@@ -9,10 +9,31 @@ public class Timber {
     private static String sMethod;
     private static int sLine;
 
+    //   ____                _                   _
+    //  / ___|___  _ __  ___| |_ _ __ _   _  ___| |_ ___  _ __
+    // | |   / _ \| '_ \/ __| __| '__| | | |/ __| __/ _ \| '__|
+    // | |___ (_) | | | \__ \ |_| |  | |_| | (__| |_ (_) | |
+    //  \____\___/|_| |_|___/\__|_|   \__,_|\___|\__\___/|_|
+    //
+
     private Timber() {
         // Disallow instantiation
     }
 
+    //  ____        _     _ _        __  __      _   _               _
+    // |  _ \ _   _| |__ | (_) ___  |  \/  | ___| |_| |__   ___   __| |___
+    // | |_) | | | | '_ \| | |/ __| | |\/| |/ _ \ __| '_ \ / _ \ / _` / __|
+    // |  __/| |_| | |_) | | | (__  | |  | |  __/ |_| | | | (_) | (_| \__ \
+    // |_|    \__,_|_.__/|_|_|\___| |_|  |_|\___|\__|_| |_|\___/ \__,_|___/
+    //
+
+    /**
+     * Logs the provided params with log level VERBOSE.  If passing in multiple Strings, separating
+     * them with a comma will improve performance.
+     *
+     * @param args comma separated list of Strings to print
+     * @param <T>
+     */
     public static <T> void v(T... args) {
         if (BuildConfig.DEBUG) {
             configureClassInfo();
@@ -20,6 +41,13 @@ public class Timber {
         }
     }
 
+    /**
+     * Logs the provided params with log level DEBUG.  If passing in multiple Strings, separating
+     * them with a comma will improve performance.
+     *
+     * @param args comma separated list of Strings to print
+     * @param <T>
+     */
     public static <T> void d(T... args) {
         if (BuildConfig.DEBUG) {
             configureClassInfo();
@@ -27,26 +55,65 @@ public class Timber {
         }
     }
 
+    /**
+     * Logs the provided params with log level INFO.  If passing in multiple Strings, separating
+     * them with a comma will improve performance.
+     *
+     * @param args comma separated list of Strings to print
+     * @param <T>
+     */
     public static <T> void i(T... args) {
         configureClassInfo();
         Log.i(sClass, generateString(args));
     }
 
+    /**
+     * Logs the provided params with log level WARN.  If passing in multiple Strings, separating
+     * them with a comma will improve performance.
+     *
+     * @param args comma separated list of Strings to print
+     * @param <T>
+     */
     public static <T> void w(T... args) {
         configureClassInfo();
         Log.w(sClass, generateString(args));
     }
 
+    /**
+     * Logs the provided params with log level ERROR.  If passing in multiple Strings, separating
+     * them with a comma will improve performance.
+     *
+     * @param args comma separated list of Strings to print
+     * @param <T>
+     */
     public static <T> void e(T... args) {
         configureClassInfo();
         Log.e(sClass, generateString(args));
     }
 
+    /**
+     * Logs the provided params with log level WTF.  If passing in multiple Strings, separating
+     * them with a comma will improve performance.
+     *
+     * @param args comma separated list of Strings to print
+     * @param <T>
+     */
     public static <T> void wtf(T... args) {
         configureClassInfo();
         Log.wtf(sClass, generateString(args));
     }
 
+    //  ____       _            _         __  __      _   _               _
+    // |  _ \ _ __(_)_   ____ _| |_ ___  |  \/  | ___| |_| |__   ___   __| |___
+    // | |_) | '__| \ \ / / _` | __/ _ \ | |\/| |/ _ \ __| '_ \ / _ \ / _` / __|
+    // |  __/| |  | |\ V / (_| | |_  __/ | |  | |  __/ |_| | | | (_) | (_| \__ \
+    // |_|   |_|  |_| \_/ \__,_|\__\___| |_|  |_|\___|\__|_| |_|\___/ \__,_|___/
+    //
+
+    /**
+     * Internal helper method to set the class name, method name, and line number where Timber was
+     * called.
+     */
     private static void configureClassInfo() {
         final StackTraceElement[] elements = new Throwable().getStackTrace();
         sClass = elements[2].getClassName();
@@ -54,6 +121,14 @@ public class Timber {
         sLine = elements[2].getLineNumber();
     }
 
+    /**
+     * Internal helper method to generate the actual string that will be logged.
+     *
+     * @param args comma separated list of Strings to print
+     * @param <T>
+     *
+     * @return the String to be logged
+     */
     private static <T> String generateString(T... args) {
         final StringBuilder sb = new StringBuilder("[" + sMethod + ":" + sLine + "] ");
         for (T obj : args) {
